584924435db7116855d69e795c77b423cc08bbd8
sohail87 <sohaildeen73@gmail.com>
2017-07-02 13:10:09 +0100

    TDD Red - VowelCount

---
+++ CodeWars/VowelCountSpecTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars
+{
+    [TestClass]
+    public class VowelCountSpecTest
+    {
+        [TestMethod]
+        public void GivenTomorrowVowelCountIs3()
+        {
+            Assert.AreEqual(3, VowelCount("Tomorrow"));
+        }
+
+        private int VowelCount(string tomorrow)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
